8068177cd23e6f07171acc14ff39252cca545a3e
bookuha <82080872+bookuha@users.noreply.github.com>
2023-03-04 17:22:13 +0200

    init

---
+++ src/Application/Services/UserService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services;
+
+public class UserService : IUserService
+{
+    public async Task<string> SayHi(int userId)
+    {
+        return await Task.FromResult($"Hi-Hi from {userId}!");
+    }
+}
---
+++ src/Infrastructure/Data/FlitchDbContext.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class FlitchDbContext : DbContext
+{
+}
