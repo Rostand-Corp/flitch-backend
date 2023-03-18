e26a0e6e4fbff8bacdeedbae085c5aba7c9cf7f0
bookuha <82080872+bookuha@users.noreply.github.com>
2023-03-18 13:44:58 +0200

    Add Color and User domain entities

---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,9 @@
+namespace Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string DisplayName { get; set; }
+    public string Status { get; set; }
+    public Color Color { get; set; }
+}
