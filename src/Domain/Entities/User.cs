66991e26018e15f1da3fda1be6813e11fef67282
bookuha <82080872+bookuha@users.noreply.github.com>
2023-03-18 13:46:44 +0200

    Add related id to the User's Color

--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -5,5 +5,7 @@ public class User
     public Guid Id { get; set; }
     public string DisplayName { get; set; }
     public string Status { get; set; }
+    
+    public Guid ColorId { get; set; }
     public Color Color { get; set; }
 }
